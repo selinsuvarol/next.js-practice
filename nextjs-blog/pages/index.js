e4cfb7ee98c0dce4202f53ff33ed9c4861380ad2
Selin Su Varol <selinsuvarol@gmail.com>
2022-07-17 16:56:04 +0300

    added navbar + tailwind

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,17 +1,17 @@
 import Head from 'next/head'
-import Navbar from '../comps/Navbar';
+import Navbar from '../comps/Navbar.jsx';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className=" bg-cyan-300 w-screen border-red-600 border">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
       <main>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
+      <h1 className="text-3xl bg-yellow-300  underline">
+        Main Content Here 
       </h1>
       </main>
     </div>
